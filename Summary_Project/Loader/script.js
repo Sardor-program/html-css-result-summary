712976cec117be6435985a3c8920ed39a091d7e5
Sardor-program <137090102+Sardor-program@users.noreply.github.com>
2023-08-12 12:29:39 +0500

    summary
    summary and results using html and css

---
+++ Summary_Project/Loader/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector('.loader')
+
+    setTimeout( () => {
+        loader.style.opacity = '0'
+        setTimeout( ()=>{
+            loader.style.display = 'none'
+        },500 )
+
+    },2000)
+})
+
+
+
+
+
+
+
+
+
+
+
